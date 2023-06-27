413a3646bbceaf2c759b85ac6ed3b074774dae17
TarcaAndreea <adrianatarca1@gmail.com>
2023-06-27 18:13:12 +0300

    js update

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const selectBtn = document.querySelector('.breed-select');
+const loading = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
+const catInfo = document.querySelector('cat-info');
+selectBtn.addEventListener(click, selectEvent);
+function selectEvent() {}
